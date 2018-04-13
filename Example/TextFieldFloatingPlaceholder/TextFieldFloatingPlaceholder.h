7df248c7a4ca07d9a80db8c1a8101e62256a11e3
chocovayashi <c.tsujiyan727@gmail.com>
2018-04-13 16:55:02 +0900

    add scheme

---
+++ Example/TextFieldFloatingPlaceholder/TextFieldFloatingPlaceholder.h
@@ -0,0 +1,19 @@
+//
+//  TextFieldFloatingPlaceholder.h
+//  TextFieldFloatingPlaceholder
+//
+//  Created by 辻林 大揮 on 2018/04/13.
+//  Copyright © 2018年 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TextFieldFloatingPlaceholder.
+FOUNDATION_EXPORT double TextFieldFloatingPlaceholderVersionNumber;
+
+//! Project version string for TextFieldFloatingPlaceholder.
+FOUNDATION_EXPORT const unsigned char TextFieldFloatingPlaceholderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextFieldFloatingPlaceholder/PublicHeader.h>
+
+
